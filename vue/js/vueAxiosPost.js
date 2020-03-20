5148c60a2c505d67e9cefc743198d2adcc786f03
Finch <garybo14@gmail.com>
2020-03-20 14:05:42 +0800

    <feature>: add stop/start btn, change scroll fun

--- vue/js/vueAxiosPost.js
+++ vue/js/vueAxiosPost.js
@@ -1,21 +1,23 @@
+// 將 axios 指定給 $http 屬性
+
 var $ApiURL = 'jsp/axiosDataGet.jsp';
-var updateFlag = true;
 Vue.prototype.$http = axios;
 var dataLength = 0;
 var dataCounter = 0;
+var scrollEnd = true;
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!',
+        message: '',
         memo: '',
-        time: 0
+        Line:0,
+        time: 0,
+        scrollEndName: 'Stop'
     },
     mounted: function counter() {
         interval = setInterval(() => {
-            if (this.time == 0) {
-                if(updateFlag){
-                    this.get_store_info();
-                }
+            if (this.time === 0) {
+                this.get_store_info();
             }
             if(this.time > 0){
                 this.time = this.time - 1;
@@ -25,30 +27,33 @@ var app = new Vue({
         }, 1000)
     },
     methods: {
-        get_store_info() {
+        get_store_info: function() {
             var self = this;
             let fmdata = 'dataLength='+dataLength+"&dataCounter="+dataCounter;
             this.$http.post($ApiURL,fmdata)
                 .then((result) => {
                     console.log(result)
                     if(result.data.line != undefined){
-                        dataCounter=result.data.line;
+                        dataCounter = result.data.line;
+                        this.Line = result.data.line;
                     }
                     if(result.data.size != undefined){
                         dataLength=result.data.size;
                     }
-                    if(result.status == 200){
-                        this.message = result.data.content;
-                    }else if(result.status == 206){
-                        this.message += result.data.content;
+                    if( result.data.content != undefined){
+                        if(this.message == ''){
+                            this.message = result.data.content;
+                        }else{
+                            this.message += result.data.content;
+                        }
                     }
-                    if( result.data.size == 1){
+                    if( result.data.size === 1){
                         this.memo = 'The data is loading, please wait..., the scroll will move to bottom when the data loading is completed.';
                         this.time = 0;
                     }else{
                         this.memo ='';
                         this.time = 5;
-                        // this.scrollToEnd();
+                        this.scrollToEnd();
                     }
                 })
                 .catch(function(error) {
@@ -56,7 +61,22 @@ var app = new Vue({
                 });
         },
         scrollToEnd: function() {
-          window.scrollTo(0,document.querySelector(".logData").scrollHeight);
+          //window.scrollTo(0,document.querySelector(".logBody").scrollHeight);
+          //document.body.scrollTop = document.body.scrollHeight;
+          if(scrollEnd){
+            let nowHeight = Math.max(document.documentElement.clientHeight,document.documentElement.scrollHeight);
+            window.scrollTo(0,nowHeight);
+          }
+
+        },
+        scrollStatusChange: function(){
+            if(this.scrollEnd){
+                this.scrollEnd = false;
+                this.scrollEndName = 'Start';
+            }else{
+                this.scrollEnd = true;
+                this.scrollEndName = 'Stop';
+            }
         },
     }
-})
+});
